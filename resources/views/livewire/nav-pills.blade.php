46e95d679648a6cc9f273360ed03bf323100018e
Oshane Bailey <b4.oshany@gmail.com>
2023-09-20 11:54:00 +0000

    close: add hide option

--- resources/views/livewire/nav-pills.blade.php
+++ resources/views/livewire/nav-pills.blade.php
@@ -1,6 +1,9 @@
 <div class="lv-nav-tabs">
     <ul class="nav nav-tabs nav-pills-header">
         @foreach ( $tabs as $key => $tab )
+            @if (!empty($tab['hide']) && $tab['hide'])
+                @continue
+            @endif
             <li class="nav-item">
                 <a class="nav-link @if ($key == $current_tab) active @endif"
                     href="?lvtab={{ $key }}#{{ $key }}"
@@ -20,17 +23,19 @@
         $content = $tabs[$current_tab];
     @endphp
     <div class="nav-pills-content py-3"  id="{{ $current_tab }}"  aria-labelledby="{{ $current_tab }}-tab">
-        @if ( !empty($content['view']) )
-            @if ( is_array($content['view']) )
-                @include($content['view'][0], $content['view'][1])
-            @elseif( is_string($content['view']) )
-                @include($content['view'])
-            @endif
-        @elseif( !empty($content['livewire']) )
-            @if ( is_array($content['livewire']) )
-                @livewire(...$content['livewire'], key($current_tab))
-            @elseif( is_string($content['livewire']) )
-                @livewire($content['livewire'], key($current_tab))
+        @if (empty($content['hide']) || (!empty($content['hide']) && ! $content['hide']))
+            @if ( !empty($content['view']) )
+                @if ( is_array($content['view']) )
+                    @include($content['view'][0], $content['view'][1])
+                @elseif( is_string($content['view']) )
+                    @include($content['view'])
+                @endif
+            @elseif( !empty($content['livewire']) )
+                @if ( is_array($content['livewire']) )
+                    @livewire(...$content['livewire'], key($current_tab))
+                @elseif( is_string($content['livewire']) )
+                    @livewire($content['livewire'], key($current_tab))
+                @endif
             @endif
         @endif
     </div>
--- resources/views/livewire/nav-tabs.blade.php
+++ resources/views/livewire/nav-tabs.blade.php
@@ -1,13 +1,13 @@
 <div class="lv-nav-tabs">
     <ul class="nav nav-tabs">
-        @foreach ( $tabs as $key => $tab )
+        @foreach ($tabs as $key => $tab)
+            @if (!empty($tab['hide']) && $tab['hide'])
+                @continue
+            @endif
             <li class="nav-item">
                 <a class="nav-link @if ($loop->first) active @endif"
-                    href="?lvtab={{ $key }}#{{ $key }}"
-                    data-toggle="tab"  role="tab"
-                    aria-controls="{{ $key }}" aria-selected="false"
-                    wire:ignore
-                   id="tab-{{$key}}"   >
+                    href="?lvtab={{ $key }}#{{ $key }}" data-toggle="tab" role="tab"
+                    aria-controls="{{ $key }}" aria-selected="false" wire:ignore id="tab-{{ $key }}">
                     {{ __($tab['title']) }}
                 </a>
             </li>
@@ -15,18 +15,23 @@
     </ul>
 
     <div class="tab-content py-3">
-        @foreach ($tabs as $key => $content )
-            <div class="tab-pane fade @if ($loop->first) show active @endif " style="padding: 0 !important;" id="{{ $key }}" role="tabpanel" aria-labelledby="{{ $key }}-tab">
-                @if ( !empty($content['view']) )
-                    @if ( is_array($content['view']) )
+        @foreach ($tabs as $key => $content)
+            @if (!empty($content['hide']) && $content['hide'])
+                @continue
+            @endif
+            <div class="tab-pane fade @if ($loop->first) show active @endif "
+                style="padding: 0 !important;" id="{{ $key }}" role="tabpanel"
+                aria-labelledby="{{ $key }}-tab">
+                @if (!empty($content['view']))
+                    @if (is_array($content['view']))
                         @include($content['view'][0], $content['view'][1])
-                    @elseif( is_string($content['view']) )
+                    @elseif(is_string($content['view']))
                         @include($content['view'])
                     @endif
-                @elseif( !empty($content['livewire']) )
-                    @if ( is_array($content['livewire']) )
+                @elseif(!empty($content['livewire']))
+                    @if (is_array($content['livewire']))
                         @livewire(...$content['livewire'], key($current_tab))
-                    @elseif( is_string($content['livewire']) )
+                    @elseif(is_string($content['livewire']))
                         @livewire($content['livewire'], key($current_tab))
                     @endif
                 @endif
